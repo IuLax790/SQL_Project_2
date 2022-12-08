9c18907a93b175bd24da1137b02aee71f4df6a48
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2022-12-08 13:10:31 +0200

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,24 @@
+create database My_Coordinates 
+use My_Coordinates
+create table Coordinates
+(
+City varchar(20),
+Country varchar(20),
+latitude varchar(15),
+longtitude varchar(15)
+)
+insert into My_Coordinates  values('Kayseri','Turkiye','35.777000N','38.444000E')
+insert into My_Coordinates  values('Istanbul','Turkiye','41.0082° N','28.9784° E')
+insert into My_Coordinates  values('Ankara','Turkiye','39.9334° N','32.8597° E')
+insert into My_Coordinates  values('Cairo','Egypt','30.0444° N','31.2357° E')
+insert into My_Coordinates  values('Moscow','Russia','55.7558° N','37.6173° E')
+insert into My_Coordinates  values('Helsinki','Finland','60.1699° N','24.9384° E')
+insert into My_Coordinates  values('Stockholm','Sweden','59.3293° N','18.0686° E')
+insert into My_Coordinates  values('Hamburg','Germany','53.5488° N','9.9872° E')
+insert into My_Coordinates  values('Paris','France','48.8566° N','2.3522° E')
+insert into My_Coordinates  values('London','UK','51.5072° N','0.1276° W')
+insert into My_Coordinates  values('New York','USA','40.7128° N','74.0060° W')
+insert into My_Coordinates  values('Miami','USA','25.7617° N','80.1918° W')
+insert into Coordinates values('Mexico City','Mexico','19.4326° N','99.1332° W')
+insert into Coordinates values('Rio de Janeiro','Brazil','22.9068° S','43.1729° W')
+insert into Coordinates values('Tierra del Fuego','Argentina','54.3084° S','67.7452° W')
---
+++ SQLQuery4.sql
@@ -0,0 +1,6 @@
+Use My_Coordinates
+SELECT * from Coordinates
+SELECT * from Coordinates where latitude = '35.777000N'
+SELECT * from Coordinates where longtitude = '18.0686° E'
+SELECT * from Coordinates WHERE city = 'Rio de Janeiro'
+SELECT * from coordinates WHERE city= 'London' or longtitude = '74.0060° W'
